04a878ac7913485c6022cc545b06220ecf26434d
snap22 <brycemine111@gmail.com>
2024-12-08 13:59:58 +0100

    initial commit

---
+++ src/test/java/com/example/loader/LoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
